c37cfd0dd9773b6802b66d3a651a84ba272c9f8f
Hanif Adams <omeizahanif@gmail.com>
2021-05-19 23:00:51 +0100

    rewrote convFahrToCelsius

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ that takes a single parameter and converts it to celsius.
  */
 
 function convertFahrToCelsius(value) {
-    let number = Number(value)
-    if (isNaN(number)) {
-        return `${value} is not a valid number but a/an ${typeof value}.`
+
+    let errorMessage = `${JSON.stringify(value)} is not a valid number but a/an ${typeof value}.`;
+    let result = `${value}` ? parseInt(value) : errorMessage;
+
+    if (isNaN(result)) {
+         return errorMessage;
     }
 
-    return parseInt((number - 32) / 1.8)
+    return ((result - 32) * 5/9).toFixed(4);
+    
 }
 
-console.log(convertFahrToCelsius(32))
+console.log(convertFahrToCelsius(80));
+/**
+ * Write a function named "checkYuGiOh" that takes a number, n, 
+ * as an argument, creates an array of numbers from 1 to n
+ * for strings that have multiple factors, use a hyphen as a separator 
+ * e.g 10 === "yu-oh", 30 === "yu-gi-oh"
+ *
+ */
+
+function checkYuGiOh(n) {
+  console.log('a')  
+}
+
+//console.log(checkYuGiOh(5))
+
